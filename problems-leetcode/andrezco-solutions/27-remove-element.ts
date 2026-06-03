1657a4cbf4bbcd18ca3023234b7acd35d4598ccd
Andres2003xd <machoandres12@gmail.com>
2026-06-03 11:42:18 -0500

    feat: agrega 5 ejercicios de LeetCode de Andres (21, 26, 27, 28, 35)

---
+++ problems-leetcode/andrezco-solutions/27-remove-element.ts
@@ -0,0 +1,14 @@
+function removeElement(nums: number[], val: number): number {
+  let insertPosition = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[insertPosition] = nums[i];
+      insertPosition++;
+    }
+  }
+
+  return insertPosition;
+}
+
+export { removeElement };
